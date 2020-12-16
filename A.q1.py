d355b0e895fce3113184566f6aae322b2e23598e
Muhammad Shahbaz Munir <45064124+shahbazmunir@users.noreply.github.com>
2020-12-16 23:53:53 +0500

    Update A.q1.py

--- A.q1.py
+++ A.q1.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
 
 # Question 1 results can be achieved in the following simpler way
-
+# import randint library
 from random import randint
-def foo(length):
+def foo(length):	# Define a subroutine
 	list = []
 	is_done = False
 	while(is_done == False):
@@ -15,5 +15,5 @@ def foo(length):
 			is_done = True
 	return list
 
-x = foo(10)
+x = foo(10)	# Call foo subroutine
 print x
